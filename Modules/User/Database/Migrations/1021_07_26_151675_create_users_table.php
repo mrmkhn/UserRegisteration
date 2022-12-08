9ad2a8060c0aa3d6b53f457ebd6f704ee9c725d2
mrmkhn <khani.mrm@gmail.com>
2022-12-08 14:26:42 +0330

    add user module

--- Modules/User/Database/Migrations/1021_07_26_151675_create_users_table.php
+++ Modules/User/Database/Migrations/1021_07_26_151675_create_users_table.php
@@ -15,47 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('firstName')->nullable();
-            $table->string('lastName')->nullable();
-            $table->string('username')->nullable();
-            $table->text('password')->nullable();
-            $table->string('mobile')->nullable();
-            $table->string('email')->nullable();
-            $table->string('bornDate')->nullable();
-            $table->string('nationalCode')->nullable();
-            $table->unsignedBigInteger('image_id')->nullable();
-            $table->boolean('accept_rules')->default(0);
-            $table->string('nickname')->nullable();
-            $table->string('referral_code')->unique();
-            $table->string('api_token')->nullable();
-            $table->enum('status',\Modules\User\Models\User::$enumStatuses)->default('active');
-            $table->boolean('special_teachers')->default(0);
-            $table->string('prefix')->nullable();
-            $table->string('english_name')->nullable();
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
-            $table->string('degree_of_education')->nullable();
-            $table->string('company_name')->nullable();
-            $table->string('company_logo')->nullable();
-            $table->string('activity_id')->nullable();
-            $table->string('city_id')->nullable();
-            $table->boolean('iran1450')->default(0);
-            $table->string('iran1450_options')->nullable();
-            $table->string('iran1450_register_date')->nullable();
-
-
-
-            $table->text('company_description')->nullable();
-            $table->text('secret_description')->nullable();
-            $table->longText('teachers_executive_records')->nullable();
-            $table->longText('teachers_scientific_records')->nullable();
-            $table->text('teacher_teaser')->nullable();
-            $table->text('teacher_banner')->nullable();
-
-            $table->boolean('updated')->default(0);
-            $table->bigInteger('create_user')->nullable();
-            $table->bigInteger('update_user')->nullable();
-            $table->bigInteger('delete_user')->nullable();
+            $table->string('mobile');
+            $table->string('nationalCode');
+            $table->bigInteger('created_by')->nullable();
+            $table->bigInteger('updated_by')->nullable();
+            $table->bigInteger('deleted_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -12,29 +12,4 @@
 */
 
 
-Route::prefix('admin')->namespace('Modules\User\Http\Controllers\Admin')->middleware('web','auth','admin-panel')->group(function() {
 
-    Route::resource('/user', 'UserController');
-    Route::get('/ajax/object/user', 'UserController@get_user_object')->name('get-user-object');
-    Route::get('/inactive_users', 'UserController@inactive_users')->name('inactive_users');
-    Route::get('/user_activation/{user}', 'UserController@user_activation')->name('user_activation');
-
-    Route::get('/admin-search-user', 'UserController@index')->name('admin-search-user');
-
-
-});
-
-
-
-
-
-Route::prefix('admin/activity')->namespace('Modules\User\Http\Controllers\Admin')->middleware('web','auth','admin-panel')->group(function() {
-
-    Route::get('/', 'ActivityController@index')->name('activity.index');
-    Route::get('/create', 'ActivityController@create')->name('activity.create');
-    Route::post('/store', 'ActivityController@store')->name('activity.store');
-    Route::get('/edit/{activity}', 'ActivityController@edit')->name('activity.edit');
-    Route::patch('/update/{activity}', 'ActivityController@update')->name('activity.update');
-    Route::delete('/destroy/{activity}', 'ActivityController@destroy')->name('activity.destroy');
-
-});
